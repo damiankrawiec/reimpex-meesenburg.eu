81bbbb94f83c0b3351db921eac447d1034ccade0
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2020-12-22 22:15:09 +0100

    Layout

--- system/default/content.php
+++ system/default/content.php
@@ -3,7 +3,7 @@
 //init require element on the content in section (object), rest part of section are include
 require_once 'content/object/object.class.php';
 
-$object = new ObjectContent($this->systemName(), $db, $this->currentLanguage, $this->admin);
+$object = new ObjectContent($this->systemName(), $db, $this->currentLanguage, $this->admin, $this->setting);
 
 $sectionData = $this->getSection($this->currentSection);
 
@@ -35,29 +35,49 @@ $label = $object->getAllLabel();
 
 </div>
 
-<div class="im-content <?php echo $this->setting['class-content']; ?>">
+<?php $divClass = ' class="'.($sectionData['class'] === '' ? 'container' : $sectionData['class']).'" id="'.$this->currentSection.'"'; ?>
 
-    <div class="<?php echo ($sectionData['class'] === '' ? 'container' : $sectionData['class']) ?>" id="<?php echo $this->currentSection; ?>">
+<div class="<?php echo $this->setting['class-content']; ?>">
 
-        <?php $object->display($sectionData['id'], $label['content']); ?>
+    <div<?php echo $divClass; ?>>
 
-        <?php
+        <?php $object->display($sectionData['id'], $label['content-top']); ?>
 
-        //Static content (when in system is "static" dir, and file is named like current url section) - if both are false static content is not display
-        //In static file should be defined class, rows, col, etc. - content fix to rest page box
-        //Name of files may only string section - check in foreach(dir) and stristr()
-        $object->displayStatic($sectionData['id']);
-
-        ?>
+        <?php $object->display($sectionData['id'], $label['content-center']); ?>
 
     </div>
 
 </div>
 
+<div<?php echo $divClass; ?>>
+
+    <?php $object->display($sectionData['id'], $label['content-bottom']); ?>
+
+    <?php
+
+    //Static content (when in system is "static" dir, and file is named like current url section) - if both are false static content is not display
+    //In static file should be defined class, rows, col, etc. - content fix to rest page box
+    //Name of files may only string section - check in foreach(dir) and stristr()
+    $object->displayStatic($sectionData['id']);
+
+    ?>
+
+</div>
+
+<div class="container-fluid p-0">
+
+    <?php $object->display($sectionData['id'], $label['map']); ?>
+
+</div>
+
 <div class="im-footer <?php echo $this->setting['class-footer']; ?>">
+
     <div class="container-fluid">
 
+        <?php $object->display($sectionData['id'], $label['footer']); ?>
+
     </div>
+
 </div>
 
 <?php
